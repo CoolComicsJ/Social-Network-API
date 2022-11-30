3cd508c3597b6d3aa3c8d96a9276a2077eb9aa29
CoolComicsJ <ziggaton@gmail.com>
2022-11-29 23:57:11 -0600

    "Added models and controllers"

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const { User } = require('../models');
+
+module.exports = {
+  getUser(req, res) {
+    User.find()
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a single comment
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user found with that id' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Create a comment
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+};
