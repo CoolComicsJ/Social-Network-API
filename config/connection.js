c313b2312e4d7dcb1fa8ee89719f8620c8acd6b3
CoolComicsJ <ziggaton@gmail.com>
2022-12-01 23:45:56 -0600

    "completing routes"

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
-
+const { connect, connection } = require('mongoose');
 // Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/SocialNetowrk', {
+ connect('mongodb://127.0.0.1/SocialNetwork', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 // Export connection
-module.exports = mongoose.connection;
+module.exports =  connection;
